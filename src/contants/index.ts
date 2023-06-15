0f0b691ed2aa833af7b44961ffa71ce760385b67
“gerald <fitzgeraldkachi@gmail.com>
2023-06-16 00:29:07 +0100

    fixed issues on buffer

--- src/contants/index.ts
+++ src/contants/index.ts
@@ -218,7 +218,7 @@ import {
         },
       ],
       image: carrent,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/geraldkachi",
     },
     {
       name: "Job IT",
@@ -239,7 +239,7 @@ import {
         },
       ],
       image: jobit,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/geraldkachi",
     },
     {
       name: "Trip Guide",
@@ -260,7 +260,7 @@ import {
         },
       ],
       image: tripguide,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/geraldkachi",
     },
   ];
 
